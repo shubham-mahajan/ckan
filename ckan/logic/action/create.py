db9b3928625addd941e41448a098609f4558b87b
Jesse Vickery <jesse.vickery@tbs-sct.gc.ca>
2026-05-07 20:00:27 +0000

    feat(misc): changelog;
    - Added change log file.

--- ckan/logic/action/create.py
+++ ckan/logic/action/create.py
@@ -1454,7 +1454,7 @@ def api_token_create(context: Context,
     :rtype: dictionary
 
     """
-    user, name = _get_or_bust(data_dict, [u'user', u'name'])
+    user, _name = _get_or_bust(data_dict, [u'user', u'name'])
 
     if model.User.get(user) is None:
         raise NotFound("User not found")
